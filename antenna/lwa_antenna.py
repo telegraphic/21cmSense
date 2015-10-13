a687f558f5e13d6429adfa9c8b31a729f2cab4fe
Danny Price <dan@thetelegraphic.com>
2015-10-13 12:58:33 -0400

    Adding functionality to lwa_antenna script
    Adding things like effective area and directivity. Some of the computed values
    are coming out dubiously high, so need to figure out why this is.
    
    The antenna area will ultimately set the size of the gridding kernel, so important
    to get it right.

--- antenna/lwa_antenna.py
+++ antenna/lwa_antenna.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pylab as plt
 import h5py
@@ -5,12 +6,17 @@ import h5py
 from scipy.interpolate import interp1d, RectBivariateSpline
 
 H5_DATA = "antenna/antenna_data.h5"
+H5_DATA_LOCAL = "antenna_data.h5"
 
 class LwaBeamPattern(object):
     """ Beam pattern class for the LWA antenna """
     def __init__(self):
-        self.h5 = h5py.File(H5_DATA)
-        self.data   = self.h5["data"][:]
+        try:
+            self.h5 = h5py.File(H5_DATA)
+            self.data   = self.h5["data"][:]
+        except:
+            self.h5 = h5py.File(H5_DATA_LOCAL)
+            self.data = self.h5["data"][:]
         self.theta  = self.h5["theta"][:]
         self.phi    = self.h5["phi"][:]
         self.freqs  = self.h5["frequency"][:]
@@ -100,7 +106,7 @@ class LwaBeamPattern(object):
     def compute_solid_angle(self):
         """ Compute the beam solid angle in steradians. """
         beam_lin = 10.0**(self.generated_beam_data / 10.0)
-        theta = np.deg2rad(self.generated_beam_theta)  + np.pi/2
+        theta = np.deg2rad(self.generated_beam_theta)  + (np.pi / 2)
         beam_weighted = beam_lin * np.sin(theta)
         bm  = np.average(beam_weighted) / np.max(beam_weighted)
 
@@ -116,7 +122,7 @@ class LwaBeamPattern(object):
         """
         beam_lin = 10.0**(self.generated_beam_data / 10.0)
         beam_lin2 = beam_lin**2
-        theta = np.deg2rad(self.generated_beam_theta)  + np.pi/2
+        theta = np.deg2rad(self.generated_beam_theta)  + (np.pi / 2)
         beam_weighted = beam_lin * np.sin(theta)
         beam_weighted2 = beam_lin2 * np.sin(theta)
         bm  = np.average(beam_weighted) / np.max(beam_weighted) 
@@ -137,12 +143,61 @@ class LwaBeamPattern(object):
         hwhm = self.generated_beam_theta[hm_idx]
         return np.abs(np.deg2rad(hwhm * 2))
 
+    def compute_bm_eff_from_fwhm(self):
+        """ COmpute Parsons Omega' value from FWHM
+
+        Notes:
+            Assumes symmetric beam.
+        """
+        beam_slice = self.generated_beam_data[self.generated_beam_data.shape[0]/2]
+        beam_lin = 10.0**(beam_slice / 10.0)
+        beam_slice2 = beam_lin**2
+
+        hm_idx = np.argmin(np.abs(beam_slice + 3.0))
+        hwhm = self.generated_beam_theta[hm_idx]
+
+        hm_idx2 = np.argmin(np.abs(beam_slice2 + 3.0))
+        hwhm2 = self.generated_beam_theta[hm_idx2]
+        fwhm = np.abs(np.deg2rad(hwhm * 2))
+        fwhm2 = np.abs(np.deg2rad(hwhm2 * 2))
+        #print fwhm, fwhm2
+        return fwhm / fwhm2
+
+    def compute_directivity(self, db=True):
+        bm = self.compute_solid_angle()
+        d  = 4*np.pi / bm
+        if db:
+            return 10*np.log10(d)
+        else:
+            return d
+
+    def compute_area(self, efficiency=1.0):
+        """ Compute effective area """
+        d = self.compute_directivity(db=False)
+        wl = self.compute_wl()
+        A_eff = d * efficiency * np.pi * wl**2
+        return A_eff
+
+    def compute_wl(self):
+        """Return wavelength"""
+        wl = 2.99e8 / (self.generated_beam_freq * 1e6)
+        return wl
+
 if __name__ == "__main__":
     lwa = LwaBeamPattern()
-    lwa.generate(60)
+    lwa.generate(50)
     #lwa.view2()
-    print lwa.compute_bm_eff()
-    print lwa.compute_solid_angle()
-    print lwa.compute_fwhm()
-        
-        
+    print "FWHM", lwa.compute_fwhm()
+    print "Solid angle from FWHM", lwa.compute_fwhm()**2
+    print "Directivity from FWHM [db]", 10*np.log10(4*np.pi / lwa.compute_fwhm()**2)
+    area_fwhm = 4*np.pi / lwa.compute_fwhm()**2 * np.pi * lwa.compute_wl()**2
+    print "Area from FWHM", area_fwhm
+    print "Effective radius", np.sqrt(area_fwhm / np.pi)
+    print "Beam eff from FWHM", lwa.compute_bm_eff_from_fwhm()
+    print "Wavelength", lwa.compute_wl()
+    print "Dish size in lambda", np.sqrt(area_fwhm / np.pi) / lwa.compute_wl()
+
+    print "Directivity", lwa.compute_directivity()
+    print "Solid angle", lwa.compute_solid_angle()
+    print "Effective area [m2]", lwa.compute_area()
+    print "Beam eff", lwa.compute_bm_eff()
